22f8017de0b0cd8b6dde92784b6f881ee61c4c44
Manuel <manu.biurrun@gmail.com>
2021-09-14 20:30:18 -0300

    anda la del indio pero le faltan cosas

---
+++ security-test/backend/src/main/java/com/thecodeveal/app/config/JWTAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package com.thecodeveal.app.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+	
+	private UserDetailsService userDetailsService;
+	private JWTTokenHelper jwtTokenHelper;
+
+	private boolean testing = true;
+	
+	public JWTAuthenticationFilter(UserDetailsService userDetailsService,JWTTokenHelper jwtTokenHelper) {
+		this.userDetailsService=userDetailsService;
+		this.jwtTokenHelper=jwtTokenHelper;
+		
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		
+		String authToken=jwtTokenHelper.getToken(request);
+
+		if(!testing) {
+			System.out.println(authToken);
+			this.testing = true;
+		} else {
+			this.testing = false;
+		}
+		
+		if(null!=authToken) {
+			
+			String userName=jwtTokenHelper.getUsernameFromToken(authToken);
+			
+			if(null!=userName) {
+				
+				UserDetails userDetails=userDetailsService.loadUserByUsername(userName);
+				
+				if(jwtTokenHelper.validateToken(authToken, userDetails)) {
+					
+					UsernamePasswordAuthenticationToken authentication=new UsernamePasswordAuthenticationToken(userDetails, null,userDetails.getAuthorities());
+					authentication.setDetails(new WebAuthenticationDetails(request));
+					
+					SecurityContextHolder.getContext().setAuthentication(authentication);
+					
+					
+					
+				}
+				
+			}
+			
+		}
+		
+		filterChain.doFilter(request, response);
+		
+		
+		
+	}
+
+}
